c66d729873a8b96b92773c8a0d969799e557b906
Ahmad Mamdouh <106310245+AhmedMamdou798@users.noreply.github.com>
2024-12-03 23:55:54 +0200

    modified code

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
-USE `alx_book_store`;  -- Replace 'alx_book_store' with your actual database name if different
+USE `alx_book_store`;
 
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-       (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-       (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+       (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+       (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
