a4f7fead524720c37d55e0bdf53bef4c7a7c0394
Ahmad Mamdouh <106310245+AhmedMamdou798@users.noreply.github.com>
2024-12-03 23:37:42 +0200

    task-5 done

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- This script inserts a single row into the customer table
+
+USE `alx_book_store`;  -- Replace with your actual database name if different
+
+INSERT INTO CUSTOMER (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
